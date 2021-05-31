00bec77305e874521faa7c0ef24ae02fd61dd54b
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2021-05-31 10:26:23 -0500

    New docs

--- makedocs.sh
+++ makedocs.sh
@@ -5,3 +5,8 @@ jazzy
 git add .
 git commit -m "New docs"
 git push
+curl -s \
+  --form-string "token=ay3gqh9e4kpd9i7bf99v4i5jxz8tia" \
+  --form-string "user=u6pezkwysz6z2iqi9ugqy5susa1osu" \
+  --form-string "message=New Docs Pushed" \
+  https://api.pushover.net/1/messages.json
