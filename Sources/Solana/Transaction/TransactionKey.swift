3a108611dd166a8aa6be00771b8204472495b2a2
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2021-06-18 13:09:22 -0500

    Web socket fixes

---
+++ Sources/Solana/Transaction/TransactionKey.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionKey.swift
+//  
+//
+//  Created by Nathan Lawrence on 6/14/21.
+//
+
+import Foundation
+
+/**
+ A key used to authorize a transaction or instruction.
+ */
+public struct TransactionKey {
+    internal init(publicKey: Base58, isSigner: Bool, isWritable: Bool) {
+        self.publicKey = publicKey
+        self.isSigner = isSigner
+        self.isWritable = isWritable
+    }
+
+    let publicKey: Base58
+    let isSigner: Bool
+    let isWritable: Bool
+}
