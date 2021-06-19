82f1767ab579ffc4b68286d3d0936846f821e1b2
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2021-06-19 12:24:10 -0500

    Add transaction safe mode

--- Sources/Solana/RPC/RPCSession.swift
+++ Sources/Solana/RPC/RPCSession.swift
@@ -14,4 +14,17 @@ public class RPCSession {
 
     public let requestAdaptor: RPCRequestAdaptor
 
+
+    /**
+     When Transaction Safe Mode is enabled, the framework will attempt to validate transactions before they are submitted to the chain. If a transaction is potentially dangerous or has inconsistent signatures, the framework will trigger a precondition failure, abort the action, and allow you to investigate the error.
+
+     By default, Transaction Safe Mode is enabled when your app is built in a debug setting and disabled in production.
+     */
+    public var transactionSafeModeEnabled: Bool = {
+        #if DEBUG
+        return true
+        #else
+        return false
+        #endif
+    }()
 }
